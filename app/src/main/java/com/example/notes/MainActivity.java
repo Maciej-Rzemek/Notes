5b587171d5848bee6bb89ff2279c02d278012bfe
Maciej Rzemek <m.rzemek7@gmail.com>
2019-02-14 23:39:06 +0100

    Commit 1

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -2,21 +2,37 @@ package com.example.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.LinearLayout;
 
+import com.example.notes.adapters.NotesRecyclerAdapter;
 import com.example.notes.models.Note;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    private RecyclerView recyclerView;
+    private ArrayList<Note> notes = new ArrayList<>();
+    private NotesRecyclerAdapter mNotesRecyclerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
 
-        Note note = new Note("title", "content", "timeStamp");
+        initRecyclerView();
+    }
 
-        Log.d(TAG, "onCreate: my Note " + note.toString());
+    private void initRecyclerView() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        mNotesRecyclerAdapter = new NotesRecyclerAdapter(notes);
+        recyclerView.setAdapter(mNotesRecyclerAdapter);
     }
 }
--- app/src/main/java/com/example/notes/adapters/NotesRecyclerAdapter.java
+++ app/src/main/java/com/example/notes/adapters/NotesRecyclerAdapter.java
@@ -1,4 +1,52 @@
 package com.example.notes.adapters;
 
-public class NotesRecyclerAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.notes.R;
+import com.example.notes.models.Note;
+
+import java.util.ArrayList;
+
+public class NotesRecyclerAdapter extends RecyclerView.Adapter<NotesRecyclerAdapter.ViewHolder> {
+
+    private ArrayList<Note> mNotes = new ArrayList<>();
+
+    public NotesRecyclerAdapter(ArrayList<Note> mNotes) {
+        this.mNotes = mNotes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_note_list_item, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        viewHolder.title.setText(mNotes.get(i).getTitle());
+        viewHolder.timestamp.setText(mNotes.get(i).getTimeStamp());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNotes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title, timestamp;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.note_title);
+            timestamp = itemView.findViewById(R.id.note_timestamp);
+        }
+    }
 }
