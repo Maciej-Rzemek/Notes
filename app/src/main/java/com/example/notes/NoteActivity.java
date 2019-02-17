3268f9705f2294208fb6dc457ca75b9827ea703a
Maciej Rzemek <m.rzemek7@gmail.com>
2019-02-17 17:44:10 +0100

    LinesEdit added

--- app/src/main/java/com/example/notes/NoteActivity.java
+++ app/src/main/java/com/example/notes/NoteActivity.java
@@ -3,45 +3,126 @@ package com.example.notes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.notes.models.Note;
 
-public class NoteActivity extends AppCompatActivity {
+public class NoteActivity extends AppCompatActivity implements View.OnTouchListener, GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+
     private static final String TAG = "NoteActivity";
+    
+    // UI components
     private LinedEditText mLinedEditText;
-    private EditText mEditText;
+    private EditText mEditTitle;
     private TextView mViewTitle;
 
+
+    // vars
     private boolean mIsNewNote;
+    private Note mNoteInitial;
+    private GestureDetector mGestureDetector;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
-        
         mLinedEditText = findViewById(R.id.note_text);
-        mEditText = findViewById(R.id.note_edit_title);
+        mEditTitle = findViewById(R.id.note_edit_title);
         mViewTitle = findViewById(R.id.note_text_title);
 
-        if (getIncomingIntent()) {
+        if(getIncomingIntent()){
             // this is a new note (EDIT MODE)
-
-        } else {
-            // this is NOT a new note (VIEW MODE)
+            setNewNoteProperties();
         }
+        else{
+            // this is note a new note (VIEW MODE)
+            setNoteProperties();
+        }
+        setListeners();
     }
 
-    private boolean getIncomingIntent() {
-        if (getIntent().hasExtra("selected_note")) {
-            Note incomingNote = getIntent().getParcelableExtra("selected_note");
+    private boolean getIncomingIntent(){
+        if(getIntent().hasExtra("selected_note")){
+            mNoteInitial = getIntent().getParcelableExtra("selected_note");
 
             mIsNewNote = false;
             return false;
         }
         mIsNewNote = true;
         return true;
+    }
+
+    private void setListeners() {
+        mLinedEditText.setOnTouchListener(this);
+        mGestureDetector = new GestureDetector(this, this);
+    }
+
+
+    private void setNewNoteProperties(){
+        mViewTitle.setText("Note Title");
+        mEditTitle.setText("Note Title");
+    }
+
+    private void setNoteProperties(){
+        mViewTitle.setText(mNoteInitial.getTitle());
+        mEditTitle.setText(mNoteInitial.getTitle());
+        mLinedEditText.setText(mNoteInitial.getContent());
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent motionEvent) {
+
+        return mGestureDetector.onTouchEvent(motionEvent);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        Log.d(TAG, "onDoubleTap: " + "On Double Tap");
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
     }
 }
